eadb7d6bd8c88391116d9c8102bed65ee7e394ab
FabioLutz <fabio_lutz@outlook.com>
2025-05-05 10:31:55 -0300

    refactor: add length field

--- pkg/dynamicarray/dynamicarray.go
+++ pkg/dynamicarray/dynamicarray.go
@@ -3,18 +3,21 @@ package dynamicarray
 import "fmt"
 
 type dynamicArray struct {
-	data []int
+	data   []int
+	length int
 }
 
 func New() *dynamicArray {
 	return &dynamicArray{
-		data: make([]int, 0),
+		data:   make([]int, 0),
+		length: 0,
 	}
 }
 
 func NewWith(values ...int) *dynamicArray {
 	return &dynamicArray{
-		data: values,
+		data:   values,
+		length: len(values),
 	}
 }
 
@@ -23,76 +26,72 @@ func (dynamicArray *dynamicArray) String() string {
 }
 
 func (dynamicArray *dynamicArray) Get(index uint) int {
-	if length := len(dynamicArray.data); int(index) >= length {
-		panic(fmt.Sprintf("failed to add: index %d out of bounds [0:%d]", index, length))
+	if int(index) >= dynamicArray.length {
+		panic(fmt.Sprintf("failed to add: index %d out of bounds [0:%d]", index, dynamicArray.length))
 	}
 	return dynamicArray.data[index]
 }
 
 func (dynamicArray *dynamicArray) Set(index uint, value int) {
-	if length := len(dynamicArray.data); int(index) >= length {
-		panic(fmt.Sprintf("failed to add: index %d out of bounds [0:%d]", index, length))
+	if int(index) >= dynamicArray.length {
+		panic(fmt.Sprintf("failed to add: index %d out of bounds [0:%d]", index, dynamicArray.length))
 	}
 
 	dynamicArray.data[index] = value
 }
 
 func (dynamicArray *dynamicArray) Add(value int) {
-	var length int = len(dynamicArray.data)
-
-	var newArray = make([]int, length+1)
+	var newArray = make([]int, dynamicArray.length+1)
 
 	copy(newArray, dynamicArray.data)
 
-	newArray[length] = value
+	newArray[dynamicArray.length] = value
 
+	dynamicArray.length++
 	dynamicArray.data = newArray
 }
 
 func (dynamicArray *dynamicArray) AddIn(index uint, value int) {
-	var length int = len(dynamicArray.data)
-
-	if int(index) >= length {
-		panic(fmt.Sprintf("failed to add: index %d out of bounds [0:%d]", index, length))
+	if int(index) >= dynamicArray.length {
+		panic(fmt.Sprintf("failed to add: index %d out of bounds [0:%d]", index, dynamicArray.length))
 	}
 
 	var newArray1 = make([]int, index+1)
-	var newArray2 = make([]int, length-int(index))
+	var newArray2 = make([]int, dynamicArray.length-int(index))
 
 	copy(newArray1, dynamicArray.data[:index])
 	copy(newArray2, dynamicArray.data[index:])
 
 	dynamicArray.data = append(newArray1, newArray2...)
 
+	dynamicArray.length++
 	dynamicArray.data[index] = value
 }
 
 func (dynamicArray *dynamicArray) RemoveFirst() {
-	var length int = len(dynamicArray.data)
-
-	if length == 0 {
+	if dynamicArray.length == 0 {
 		panic("failed to remove value: dynamic array is empty")
 	}
 
-	var newArray = make([]int, length-1)
+	var newArray = make([]int, dynamicArray.length-1)
 
-	copy(newArray, dynamicArray.data[:length-1])
+	copy(newArray, dynamicArray.data[:dynamicArray.length-1])
 
+	dynamicArray.length--
 	dynamicArray.data = newArray
 }
 
 func (dynamicArray *dynamicArray) RemoveIn(index uint) {
-	var length int = len(dynamicArray.data)
-
-	if int(index) >= length {
-		panic(fmt.Sprintf("failed to add: index %d out of bounds [0:%d]", index, length))
+	if int(index) >= dynamicArray.length {
+		panic(fmt.Sprintf("failed to add: index %d out of bounds [0:%d]", index, dynamicArray.length))
 	}
 
 	var newArray1 = make([]int, index)
-	var newArray2 = make([]int, length-int(index)-1)
+	var newArray2 = make([]int, dynamicArray.length-int(index)-1)
 
 	copy(newArray1, dynamicArray.data[:index])
 	copy(newArray2, dynamicArray.data[index+1:])
 
+	dynamicArray.length--
 	dynamicArray.data = append(newArray1, newArray2...)
 }

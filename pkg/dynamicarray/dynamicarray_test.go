5577a1926dd69f1a9950698f31f36d1c90133165
FabioLutz <135400867+FabioLutz@users.noreply.github.com>
2025-05-23 16:49:20 -0300

    test(dynamic-array): add test for NewWith

--- pkg/dynamicarray/dynamicarray_test.go
+++ pkg/dynamicarray/dynamicarray_test.go
@@ -37,3 +37,22 @@ func TestNewLength(t *testing.T) {
 		t.Errorf("Incorrect data: Expected %v, Actual %v", expectedData, actualArray.data)
 	}
 }
+
+func TestNewWith(t *testing.T) {
+	expectedData := []int{1, 2, 3, 4, 5, 6}
+	expectedLength := len(expectedData)
+
+	actualArray := NewWith(expectedData...)
+
+	if !reflect.DeepEqual(actualArray.data, expectedData) {
+		t.Errorf("Incorrect data: Expected %v, Actual %v", expectedData, actualArray.data)
+	}
+
+	if len(actualArray.data) != expectedLength {
+		t.Errorf("Incorrect array length: Expected %d, Actual %d", expectedLength, len(actualArray.data))
+	}
+
+	if actualArray.length != expectedLength {
+		t.Errorf("Incorrect length field: Expected %d, Actual %d", expectedLength, actualArray.length)
+	}
+}

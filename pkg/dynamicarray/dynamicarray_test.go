ee316fb0de04acff7454dc1cad60b91211405ee3
FabioLutz <135400867+FabioLutz@users.noreply.github.com>
2025-05-25 22:14:51 -0300

    test(dynamic-array): add test for Set

--- pkg/dynamicarray/dynamicarray_test.go
+++ pkg/dynamicarray/dynamicarray_test.go
@@ -100,3 +100,47 @@ func TestGet(t *testing.T) {
 		})
 	}
 }
+
+func TestSet(t *testing.T) {
+	var tests = []struct {
+		name        string
+		array       dynamicArray
+		inputIndex  int
+		expected    int
+		expectPanic bool
+		panicMsg    string
+	}{
+		{"valid array", dynamicArray{[]int{4, 2, 6, 3, 7}, 5}, 4, 32, false, ""},
+		{"empty array", dynamicArray{make([]int, 0), 0}, 0, -1, true, "failed to set value: index 0 out of bounds [0:0]"},
+		{"out of range", dynamicArray{[]int{1, 2, 3}, 3}, 10, -1, true, "failed to set value: index 10 out of bounds [0:3]"},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			defer func() {
+				if recover := recover(); recover != nil {
+					if !tt.expectPanic {
+						t.Errorf("Unexpected Panic: %v", recover)
+					}
+					if message, ok := recover.(string); ok {
+						if message != tt.panicMsg {
+							t.Errorf("Incorrect panic message: Expected %s, Actual %s", tt.panicMsg, message)
+						}
+					} else {
+						t.Errorf("Panic is not a string: %v", recover)
+					}
+				} else {
+					if tt.expectPanic {
+						t.Errorf("Expected error, none received")
+					}
+				}
+			}()
+
+			tt.array.Set(uint(tt.inputIndex), tt.expected)
+
+			if tt.array.data[tt.inputIndex] != tt.expected {
+				t.Errorf("Incorrect value: Expected %d, Actual %d", tt.expected, tt.array.data[tt.inputIndex])
+			}
+		})
+	}
+}
